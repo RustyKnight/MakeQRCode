b3af04cf2aeb85fe413faedc66c300679e65b0d9
Shane Whitehead <shane.whitehead@beamcommunications.com>
2018-09-05 17:17:15 +1000

    updates

--- MakeQRCode/main.swift
+++ MakeQRCode/main.swift
@@ -7,6 +7,47 @@
 //
 
 import Foundation
+import CoreGraphics
+import CoreImage
 
-print("Hello, World!")
+func convertCIImageToCGImage(_ inputImage: CIImage) -> CGImage? {
+	let context = CIContext(options: nil)
+	return context.createCGImage(inputImage, from: inputImage.extent)
+}
 
+func makeItSo() {
+	guard let url = URL(string: "shoutout://?invitetoken=1234567890abcdefghi") else {
+		print("Invalid URL")
+		return
+	}
+	guard var qrCode = QRCode(url) else {
+		print("Could not generate QRCode from URL")
+		return
+	}
+	qrCode.errorCorrection = .High
+	guard let ciImage = qrCode.ciImage else {
+		print("Could not generate CoreImage for QRCode")
+		return
+	}
+	guard let cgImage = convertCIImageToCGImage(ciImage) else {
+		print("Could not generate CoreGraphicsImage for QRCode")
+		return
+	}
+
+	let cd = FileManager.default.currentDirectoryPath
+	let fileURL = URL(fileURLWithPath: cd).appendingPathComponent("InviteQRCode.png")
+
+	guard let destination = CGImageDestinationCreateWithURL(fileURL as CFURL, kUTTypePNG, 1, nil) else {
+		print("Could not generate image destination")
+		return
+	}
+	CGImageDestinationAddImage(destination, cgImage, nil)
+	guard CGImageDestinationFinalize(destination) else {
+		print("Failed to finalise image")
+		return
+	}
+	
+	print("Image was saved to \(fileURL)")
+}
+
+makeItSo()
