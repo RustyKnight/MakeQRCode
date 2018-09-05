5a6f68e32152709c93b2bfeff1f88d05fd2013c2
Shane Whitehead <shane.whitehead@beamcommunications.com>
2018-09-05 16:58:36 +1000

    Initial Commit

---
+++ MakeQRCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MakeQRCode
+//
+//  Created by Shane Whitehead on 5/9/18.
+//  Copyright © 2018 KaiZen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
